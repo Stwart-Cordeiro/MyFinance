c84647ea0fe44e3b6c5e26e9c62ad6a1eab0befc
Stwart Douglas <stwart-douglas@hotmail.com>
2021-04-26 16:32:01 -0300

    Ajuste para vs 2019

---
+++ MyFinance.Infrastructure.CrossCutting.Identity/Model/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyFinance.Infrastructure.CrossCutting.Identity.Model
+{
+    public class UserRole : IdentityUserRole<string>
+    {
+        public User User { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
