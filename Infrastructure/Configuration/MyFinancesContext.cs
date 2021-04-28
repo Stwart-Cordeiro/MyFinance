5f05853532647b367e527e9f9e3c1e758bda6bc3
Stwart Douglas <stwart-douglas@hotmail.com>
2021-04-28 02:34:37 -0300

    Inicio do Projeto de Usuario

---
+++ Infrastructure/Configuration/MyFinancesContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class MyFinancesContext : IdentityDbContext<Usuario>
+    {
+        public MyFinancesContext(DbContextOptions<MyFinancesContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Usuario>().ToTable("AspNetUsers").HasKey(x => x.Id);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
