b3503388a742102a8df5bc3ab435b18caebd7fcf
Stwart Douglas <stwart-douglas@hotmail.com>
2021-10-12 22:59:41 -0300

    Ajuste de Menu Home

--- Domain.Core/Interfaces/Repositorys/IRepositoryLogSistema.cs
+++ Domain.Core/Interfaces/Repositorys/IRepositoryLogSistema.cs
@@ -6,6 +6,6 @@ namespace Domain.Core.Interfaces.Repositorys
     public interface IRepositoryLogSistema: IRepositoryBase<LogSistema>
     {
         public IEnumerable<LogSistema> GetAll(string userId);
-        public LogSistema GetById(string id);
+        public new LogSistema GetById(string id);
     }
 }
